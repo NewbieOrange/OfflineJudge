2d734974c7eca0ab05ca6c1b6bf6c48613916c0c
NewbieOrange <yutiancheng1999@gmail.com>
2019-03-09 00:02:48 +0800

    Cleanup main class

--- src/main/java/xyz/chengzi/offlinejudge/OfflineJudge.java
+++ src/main/java/xyz/chengzi/offlinejudge/OfflineJudge.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class OfflineJudge {
     public static final InputStream CONSOLE_IN = System.in;
