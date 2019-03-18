f86270dce3c10bd01f7db29b470fbe8b03733477
NewbieOrange <yutiancheng1999@gmail.com>
2019-03-18 22:48:41 +0800

    Now support judging multiple students

--- src/main/java/xyz/chengzi/offlinejudge/OfflineJudge.java
+++ src/main/java/xyz/chengzi/offlinejudge/OfflineJudge.java
@@ -5,33 +5,55 @@ import xyz.chengzi.offlinejudge.data.ProblemSet;
 import xyz.chengzi.offlinejudge.runtime.Judge;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.nio.file.Files;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class OfflineJudge {
     public static final InputStream CONSOLE_IN = System.in;
     public static final PrintStream CONSOLE_OUT = System.out;
     public static final PrintStream CONSOLE_ERR = System.err;
+    public static Scanner SCANNER;
 
     public static void main(String[] args) throws Exception {
         ProblemSet problemSet = ProblemSet.loadFromFolder(new File(args[0]));
-        int score = 0, fullScore = 0;
+        for (File subFolder : new File("student/").listFiles()) {
+            String studentName = subFolder.getName();
+            judge(problemSet, studentName);
+        }
+    }
 
-        System.out.println("Judged by OfflineJudge: " + new Date());
+    public static void judge(ProblemSet problemSet, String studentName) throws Exception {
+        System.out.println("Judged by OfflineJudge: " + new Date() + ", " + studentName);
         System.out.println("--> https://github.com/NewbieOrange/OfflineJudge");
+        int score = 0;
         for (Problem problem : problemSet) {
-            Judge judge = new Judge(problem, new File("student/" + problem.getName() + ".java"));
+            File srcFile = new File("student/" + studentName + "/Submission attachment(s)/" + problem.getName() + ".java");
+            overrideScanner(srcFile);
+            Judge judge = new Judge(problem, srcFile);
             List<Judge.JudgeResult> judgeResults = judge.judge();
-            for (Judge.JudgeResult judgeResult : judgeResults) {
-                if (judgeResult == Judge.JudgeResult.ACCEPTED) {
-                    score += 5;
+            if (problem.getName().equals("A1Q5") && judgeResults.get(0) == Judge.JudgeResult.ACCEPTED) {
+                score += 20;
+            } else {
+                for (Judge.JudgeResult judgeResult : judgeResults) {
+                    if (judgeResult == Judge.JudgeResult.ACCEPTED) {
+                        score += 4;
+                    }
                 }
             }
-            fullScore += 5 * problem.getTestCases().size();
             System.out.println(problem.getName() + ": " + judgeResults);
         }
-        System.out.println("Total Assignment Score: " + score + " / " + fullScore);
+        System.out.println("Total Assignment Score: " + score + " / " + 100);
+    }
+
+    public static void overrideScanner(File file) throws Exception {
+        Files.writeString(file.toPath(), Files.readString(file.toPath())
+                .replace("new Scanner(System.in)", "xyz.chengzi.offlinejudge.OfflineJudge.SCANNER"));
     }
 }
